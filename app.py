ba853805dfa14e2334a30779fce7b679bdebf40d
Anwesha Thakur <anweshathakur12@gmail.com>
2026-02-22 09:54:05 +0530

    Modify app run configuration for deployment
    Updated app to run on host and port from environment variables.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 import pandas as pd
 import joblib
@@ -38,20 +39,15 @@ def predict():
         arrival_min = int(pd.to_datetime(arrival_time, format="%H:%M").minute)
 
         # Calculate Duration
-        # Simple valid logic: duration in minutes
-        # Since we don't have arrival date, we assume:
-        # if arrival < dep, it's next day (+24h). This is a safe heuristic for short flights or user understands inputs.
         dep_total_min = dep_hour * 60 + dep_min
         arr_total_min = arrival_hour * 60 + arrival_min
-        
+
         if arr_total_min < dep_total_min:
             arr_total_min += 24 * 60
-            
+
         duration_min = arr_total_min - dep_total_min
 
         # Create DataFrame for model
-        # columns must match training data features
-        
         data = {
             'Total_Stops': [total_stops],
             'Journey_day': [journey_day],
@@ -65,14 +61,13 @@ def predict():
             'Source': [source],
             'Destination': [destination]
         }
-        
+
         df = pd.DataFrame(data)
 
         # Predict
         prediction = model.predict(df)
         output = round(prediction[0], 2)
-
         return render_template('index.html', prediction_text='Your Flight Price is Rs. {}'.format(output))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
